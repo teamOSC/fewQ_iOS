c0e5794417f6b3e83e3fb7590b94bcb941e02e48
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-07-20 00:45:39 +0530

    Beacon details added

---
+++ valet/LandingPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  LandingPageViewController.h
+//  valet
+//
+//  Created by Robin Malhotra on 20/07/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <CoreLocation/CoreLocation.h>
+@interface LandingPageViewController : UIViewController
+@property (strong, nonatomic) CLBeaconRegion *beaconRegion;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@end
---
+++ valet/LandingPageViewController.m
@@ -0,0 +1,52 @@
+//
+//  LandingPageViewController.m
+//  valet
+//
+//  Created by Robin Malhotra on 20/07/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import "LandingPageViewController.h"
+
+@interface LandingPageViewController ()
+
+@end
+
+@implementation LandingPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.navigationItem setHidesBackButton:YES];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+
+@end
